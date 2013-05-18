ec7d1a533aab8c4c53bb3e05474a3b19053e2eeb
Yarimizu14 <yarimizu.kensei14@gmail.com>
2013-05-19 04:14:54 +0900

    added focus funcion

--- js/view.js
+++ js/view.js
@@ -5,6 +5,7 @@ View.prototype = {
 	_el     : null,
 	_parent : null,
 	_handlers : [],
+	_active : null,
 	init : function(id) {
 		this._el = document.getElementById(id);
 		return this._el;
@@ -18,6 +19,16 @@ View.prototype = {
 var ViewHolder = function() { };
 ViewHolder.prototype = new View();
 ViewHolder.prototype.list = [];
+ViewHolder.prototype.initEvent = function() {
+	var self = this;
+	this._handlers["click"] = function(e) {
+		var memo_id = e.target.getAttribute("data-num");
+		self.focus(memo_id);
+		self.renderAsList();
+	};
+
+	this._el.addEventListener("click", this._handlers["click"], false);
+};
 ViewHolder.prototype.appendToList = function(ViewList) {
 	this._el.innerHTML = "";
 	var fragment = document.createDocumentFragment();
@@ -30,6 +41,8 @@ ViewHolder.prototype.renderAsList = function() {
 	this._el.innerHTML = "";
 	var fragment = document.createDocumentFragment();
 	for(var i=0; i < this.list.length; i++) {
+		this.list[i].setId(i);
+		this.list[i].render();
 		fragment.appendChild(this.list[i]._el);
 	};
 	this._el.appendChild(fragment);	
@@ -43,21 +56,35 @@ ViewHolder.prototype.del = function() {
 	};
 	this.list = arr;
 };
-
+ViewHolder.prototype.focus = function(id) {
+	var isFlag = false;
+	for (var i=0; i < this.list.length; i++) {
+		if(!isFlag && this.list[i].getId() === id) {
+			this.list[i]._active = true;
+			isFlag = true;
+			console.log(this.list[i].getId());
+		} else {
+			this.list[i]._active = false;
+		};
+	};
+};
 
 /**********************/
 var MemoTitleView = function() { };
 MemoTitleView.prototype = new View();
 MemoTitleView.prototype._active = false;
 MemoTitleView.prototype.initEvent = function() {
+	/*
 	var self = this;
+
 	this._handlers["click"] = function() {
 		self._active = !self._active;
 		self.render();
 		console.log(self._active);
 	};
-
+	
 	this._el.addEventListener("click", this._handlers["click"], false);
+	*/
 };
 MemoTitleView.prototype.create = function() {
 	this._el = document.createElement("li");
@@ -75,6 +102,12 @@ MemoTitleView.prototype.render = function() {
 		this._el.style.color = "black";
 	};
 };
+MemoTitleView.prototype.setId = function(id) {
+	this._el.setAttribute("data-num", id);
+}
+MemoTitleView.prototype.getId = function() {
+	return this._el.getAttribute("data-num");
+}
 
 /**********************/
 
